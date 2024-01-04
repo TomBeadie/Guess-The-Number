7a626ce17a70a64a251ff640cf002bddb7d78a3d
Tom <tom@atomic82.com>
2024-01-04 14:35:42 -0500

    Implement renderWin functionality

--- js/app.js
+++ js/app.js
@@ -34,41 +34,64 @@ function checkGuess(guess) {
   console.log(guess, " current guess");
   guessInput.value = "";
   if (isNaN(guess) || guess < 1 || guess > 100) {
-    renderError('Please enter a number between 1 and 100')
-    return
+    renderError("Please enter a number between 1 and 100");
+    return;
   } else if (guess === secretNum) {
     // Winner
-    isWinner = true
+    isWinner = true;
   }
   guessList.push(guess);
-  console.log(guessList)
-  render()
+  console.log(guessList);
+  render();
 }
 
 function render() {
-    // Using state to get last guess
-    const lastGuess = guessList[guessList.length - 1]
-    // Creating a new div
-    const div = document.createElement('div')
-    // Setting the textContent of the div to hold the guess
-    div.textContent = lastGuess
+  // Using state to get last guess
+  const lastGuess = guessList[guessList.length - 1];
+  // Creating a new div
+  const div = document.createElement("div");
+  // Setting the textContent of the div to hold the guess
+  div.textContent = lastGuess;
 
-    // Display the reset button and the prev guesses text
-    // If it is the first guess (subsequent guesses will already exist)
+  // Display the reset button and the prev guesses text
+  // If it is the first guess (subsequent guesses will already exist)
 
-    if (guessList.length === 1) {
-        prevGuessMsg.textContent = 'Previous Guesses:'
-        resetBtn.removeAttribute('hidden')
-    }
-    console.log(div)
+  if (guessList.length === 1) {
+    prevGuessMsg.textContent = "Previous Guesses:";
+    resetBtn.removeAttribute("hidden");
+  }
+
+  // If the guess is the secret number (if isWinner is true)
+  if (isWinner) {
+    // Render a win
+    renderWin(div);
+  } else {
+    // If no winner, render the guess
+    renderGuess(div, lastGuess);
+  }
 }
 
+function renderWin(div) {
+    messageEl.className = 'winner'
+    div.className = 'winner'
+    guessesEl.appendChild(div)
+
+    if (guessList.length === 1) {
+        messageEl.textContent = `You found the number ${secretNum} in ${guessList.length} guess!`
+    } else {
+        messageEl.textContent = `You found the number ${secretNum} in ${guessList.length} guesses`
+    }
+}
+
+function renderGuess(div, lastGuess) {}
+
 function renderError(error) {
-    messageEl.className = 'error'
-    messageEl.textContent = 'error'
+  messageEl.className = "error";
+  messageEl.textContent = "error";
 }
 
 function init() {
+  messageEl.className = "";
   guessesEl.textContent = "";
   messageEl.textContent = "Guess a number between 1 and 100.";
   resetBtn.setAttribute("hidden", true);
