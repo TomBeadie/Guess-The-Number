d12660db73600642db28c4e6e18dcb280d7516c4
Tom <tom@atomic82.com>
2024-01-04 07:11:01 -0500

    Write out cached element references

--- js/app.js
+++ js/app.js
@@ -6,8 +6,19 @@
 
 /*-------- Variables (State) --------*/
 
+let secretNum, guessList, isWinner
+
 
 /*-------- Cached Element References --------*/
+
+const form = document.querySelector('form')
+const guessInput = document.querySelector('#guess-input')
+const guessesEl = document.querySelector('#prev-guesses')
+const messageEl = document.querySelector('#message')
+const resetBtn = document.querySelector('#reset-button')
+const prevGuessMsg = document.querySelector('#prev-guesses-msg')
+
+
 
 
 /*-------- Event Listeners --------*/
